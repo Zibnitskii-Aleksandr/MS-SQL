55feb58e5b47ed9b45f3e0cd09cf2772061befb0
AleZ <40720950+Zibnitskii-Aleksandr@users.noreply.github.com>
2020-01-11 22:30:43 +0200

    Update CleanMemorySQL.sql

--- CleanMemorySQL.sql
+++ CleanMemorySQL.sql
@@ -3,6 +3,6 @@
 # Рекомендуется делать не реже 1 раза в 8-12 часов,
 # на нагруженных проектах можно делать запуск чаще!
 #
-#dbcc freeproccache
+dbcc freeproccache
 dbcc dropcleanbuffers
 dbcc freesystemcache ('ALL') with MARK_IN_USE_FOR_REMOVAL
